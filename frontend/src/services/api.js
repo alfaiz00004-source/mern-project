1ec2fe8d324fcee18c57f80a336ccf9ac202f374
Alfaiz Ansari <alfaiz00004@gmail.com>
2026-03-29 20:16:06 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://mern-project-gzqy.onrender.com/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
